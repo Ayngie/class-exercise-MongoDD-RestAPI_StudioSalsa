eed26c1c2c0355229bbce591e7c9de726daa8578
Angie <angelicareutersward@hotmail.com>
2023-02-09 15:36:29 +0100

    routes för bookings

--- src/routes/restaurantRoutes.js
+++ src/routes/restaurantRoutes.js
@@ -3,30 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  getAllRestaurants,
-  getRestaurantById,
-  createNewRestaurant,
-  updateRestaurant,
-  deleteRestaurant,
-} = require("../controllers/restaurantController");
+  getAllBookings,
+  getBookingById,
+  createNewBooking,
+  updateBooking,
+  deleteBooking,
+} = require("../controllers/bookingController");
 
-const {
-  createNewReview,
-  getAllReviews,
-} = require("../controllers/reviewController");
-
-router.get("/", getAllRestaurants);
-router.get("/:restaurantId", getRestaurantById);
-router.post("/", createNewRestaurant);
-router.put("/:restaurantId", updateRestaurant);
-router.delete("/:restaurantId", deleteRestaurant);
-//detta :restaurantId är ett dynamiskt värde och det är vår req.params.restaurantId !
-//express syntax säger att det är ett dynamiskt värde när vi säger : (kolon) före...
-//samma slags metod (tex en get) kan bara ha ett dynamiskt värde, annars om det finns två så tar den alltid den första som matchar (den ser båda som likadana, dvs den första är den som alltid tittas på).
-
-//Nästlade routes:
-router.post("/restaurants/:restaurantId/reviews/", createNewReview);
-router.get("/restaurants/:restaurantId/reviews/", getAllReviews);
+router.get("/", getAllBookings);
+router.get("/:restaurantId", getBookingById);
+router.post("/", createNewBooking);
+router.put("/:restaurantId", updateBooking);
+router.delete("/:restaurantId", deleteBooking);
 module.exports = router;
 
 //
